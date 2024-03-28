5d6301c121658bec91a70563944166103906b30c
Truong Phuc <phucb2110094@gmail.com>
2024-03-28 13:52:48 +0700

    Them lien he

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server:{
     port:3001,
     proxy: {
-      "/api/contacts/": {
+      "/api": {
         target: "http://localhost:3000/",
         changeOrigin: true,
       },
